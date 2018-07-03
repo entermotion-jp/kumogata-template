a9f5d8e821d29e54177cdf92eec0c6490944515c
n_nakazawa <n_nakazawa@entermotion.jp>
2018-07-03 12:18:17 +0900

    Support events rule
    Update gemfile

--- template/lambda-permission.rb
+++ template/lambda-permission.rb
@@ -8,23 +8,28 @@ require 'kumogata/template/iam'
 name = _resource_name(args[:name], "lambda permission")
 action = args[:action] || "lambda:*"
 function = _ref_attr_string("function", "Arn", args, "lambda function", 'arn')
-principal =
-  case args[:principal]
-  when /s3/
-    's3.amazonaws.com'
-  when /sns/
-    'sns.amazonaws.com'
-  else
-    'sns.amazonaws.com'
-  end
+principal = args[:principal]
 source_account = _ref_string("source_account", args, "account id")
 source_account = _ref_pseudo('account id') if source_account.empty?
-source_arn =
-  if principal =~ /s3/
-    _ref_attr_string('source_arn', 'Arn', args, 'bucket')
+source_arn_prefix =
+  case principal
+  when 's3'
+    'bucket'
+  when 'sns'
+    'topic'
+  when 'events'
+    'events rule'
   else
-    _ref_string("source_arn", args, 'topic')
+    ''
   end
+source_arn =
+  case principal
+  when 'sns'
+    _ref_string("source_arn", args, source_arn)
+  else
+    _ref_attr_string('source_arn', 'Arn', args, source_arn_prefix)
+  end
+
 source_arn = _iam_arn("s3", { ref: args[:ref_iam_source_arn] }) if args.key? :ref_iam_source_arn and principal =~ /s3/ and source_arn.empty?
 depends = _depends([ { ref_function: 'lambda function' } ], args)
 
@@ -33,7 +38,7 @@ _(name) do
   Properties do
     Action action
     FunctionName function
-    Principal principal
+    Principal "#{principal}.#{DOMAIN}"
     SourceAccount source_account unless source_account.empty?
     SourceArn source_arn unless source_arn.empty?
   end
