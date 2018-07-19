5e458061c08f81f77aa186c1e7f095319aa07dc0
Matsumoto <t_matsumoto@entermotion.jp>
2018-07-19 14:20:01 +0900

    support EcsParameters in properties of AWS::Events::Rule.Target element

--- lib/kumogata/template/events.rb
+++ lib/kumogata/template/events.rb
@@ -26,6 +26,10 @@ def _events_targets(args)
       Input target[:input] if target.key? :input
       InputPath target[:path] if target.key? :path
       RoleArn role unless role.empty?
+      EcsParameters _{
+        TaskCount target[:ecs_parameters][:task_count] if target[:ecs_parameters].key? :task_count
+        TaskDefinitionArn _ref_string("task_definition", target[:ecs_parameters], "ecs task definition", "arn")
+      } if target.key? :ecs_parameters
     }
   end
 end
